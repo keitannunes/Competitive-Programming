cac3eda7dffab791172707342f4a75b24266380e
keitannunes <keitannunes@gmail.com>
2023-01-26 12:44:29 -0500

    rearranged folders

---
+++ COCI/COCI06C6P1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    map<string,int> m;
+    int totalTaken = 0;
+    int count = 0; //scold
+    string current;
+    for (int i = 0; i < N; i++) {
+        cin >> current;
+        if (m[current]++ > (totalTaken - m[current] + 1) && totalTaken != 0) {
+            count++;
+        }
+        totalTaken++;
+    }
+    cout << count;
+}
+
