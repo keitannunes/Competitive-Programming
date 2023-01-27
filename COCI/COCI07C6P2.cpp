cd17c16909d271d9db14c07af73fcda1fac56ba0
keitannunes <keitannunes@gmail.com>
2023-01-26 19:30:30 -0500

    Uploaded COCI07C6P2

--- COCI/COCI07C6P2.cpp
+++ COCI/COCI07C6P2.cpp
@@ -8,13 +8,10 @@ using namespace std;
 
 int main() {
     int N, L, lastDistance, time = lastDistance = 0;
-    cin >> N;
-    cin >> L;
+    cin >> N >> L;
     for (int i = 0; i < N; i++) {
         int D,R,G;
-        cin >> D;
-        cin >> R;
-        cin >> G;
+        cin >> D >> R >> G;
         time += D-lastDistance;
         lastDistance = D;
         if (time%(R+G) < R) {
